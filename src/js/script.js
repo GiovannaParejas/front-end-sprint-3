28cc246b519b4407184182b79f60a25d750d0ed4
Queennothing33 <gparejas23@outlook.com>
2026-08-20 08:49:37 -0300

    feat: adicionando seção galeria

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,23 @@ const checkbox = document.querySelector(".menu-botao");
 checkbox.addEventListener("change", () => {
   menu.classList.toggle("menu-aberto", checkbox.checked);
 });
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 2,
+  spaceBetween: 16,
+
+    autoplay: {
+    delay: 2500,           
+    disableOnInteraction: false,  
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    576: { slidesPerView: 3, spaceBetween: 20 },
+    992: { slidesPerView: 4, spaceBetween: 24 },
+  },
+});
